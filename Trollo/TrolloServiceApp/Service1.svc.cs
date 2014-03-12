5dff34fe6de1fe1a48c7fccb74a5e5dab219aac3
Amna Maksumic <max.amna@gmail.com>
2014-03-12 18:14:13 +0100

    Dodan WCF projekat
    Dodana 2 servisa za kreiranje i brisanje korisnika

---
+++ Trollo/TrolloServiceApp/Service1.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TrolloServiceApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+       public  bool CreateUser(string username, string password, DateTime date)
+        {
+            //using (SampleDbEntities entities = new SampleDbEntities())
+            //{
+            //    Book book = new Book { BookName = name };
+            //    entities.Books.AddObject(book);
+            //    entities.SaveChanges();
+            //}
+            return true;
+
+        }
+
+
+       public  bool RenameList(string newTitle, int listID, int userID) {
+            return true; 
+        }
+
+       //public  bool CreateList(string title, int listOwner) {
+       //     return true; 
+       // }
+    }
+}
---
+++ Trollo/TrolloServiceApp/ServiceUser.svc.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TrolloServiceApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service2" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service2.svc or Service2.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceUser : IServiceUser
+    {
+
+       public bool CreateUser(User korisnik)
+        {
+
+            try
+            {
+
+                mydbEntities ent = new mydbEntities();
+
+                user kor = new user
+                {
+                    idUser = 1,
+                    username = "asdad",
+                    password = "asda"
+                };
+
+                ent.user.Add(kor);
+                ent.SaveChanges();
+
+                return true;
+            }
+
+            catch (Exception exc)
+            {
+                return false;
+            }
+            
+        }
+
+
+       public bool UpdateUser(int idUsera, string password)
+       {
+
+           try
+           {
+
+               mydbEntities ent = new mydbEntities();
+
+              
+
+               user kor = ent.user.Find(idUsera);
+
+               ent.user.Remove(kor);
+
+              
+               ent.SaveChanges();
+
+               return true;
+           }
+
+           catch (Exception exc)
+           {
+               return false;
+           }
+
+       }
+       
+    }
+}
+
