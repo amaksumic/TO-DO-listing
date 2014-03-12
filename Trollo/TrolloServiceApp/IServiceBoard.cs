8d4b26b7cc8bab73a507a20c2685ff7dc69394c2
Mensur <mensur.mandzuka@gmail.com>
2014-03-12 18:46:03 +0100

    Dodane metode za dodavanja i brisanje board-a.

---
+++ Trollo/TrolloServiceApp/IServiceBoard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TrolloServiceApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceBoard" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceBoard
+    {
+        [OperationContract]
+        bool CreateBoard(board projekat_u);
+
+        [OperationContract]
+        bool DeleteBoard(int idBoarda);
+
+    }
+
+        [DataContract]
+        public class Board
+    {
+        public Board()
+        {
+            this.list = new HashSet<list>();
+            this.user1 = new HashSet<user>();
+        }
+    
+        public int idBoard { get; set; }
+        public string title { get; set; }
+        public System.DateTime creationDate { get; set; }
+        public int boardOwner { get; set; }
+    
+        public virtual user user { get; set; }
+        public virtual ICollection<list> list { get; set; }
+        public virtual ICollection<user> user1 { get; set; }
+    }
+}
+
+
