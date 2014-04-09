1c204c2b92860eb762b131ff46791dd051e166a3
Mensur <mensur.mandzuka@gmail.com>
2014-04-09 14:29:11 +0200

    Dodana 2 grafa

--- Trollo/Trollo/Controllers/ListController.cs
+++ Trollo/Trollo/Controllers/ListController.cs
@@ -6,14 +6,83 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using DotNet.Highcharts;
+using DotNet.Highcharts.Enums;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
+
 namespace Trollo.Controllers
 {
     public class ListController : Controller
     {
         private mydbEntities db = new mydbEntities();
-       
-       
 
+
+        public ActionResult Chart1()
+        {
+            var list1 = db.list.Where(u => u.ownerBoard == 1).Count();
+            var list2 = db.list.Where(u => u.ownerBoard == 2).Count();
+
+            //Create chart Model
+            var chart1 = new Highcharts("Chart1");
+            chart1
+                .InitChart(new Chart() { DefaultSeriesType = ChartTypes.Bar })
+                .SetTitle(new Title() { Text = "Owner board 1/2" })
+
+                .SetYAxis(new YAxis() { Title = new YAxisTitle { Text = "Number of lists" } })
+                .SetSeries(new[]{
+                new Series{                   
+                    Name = "Owner board 1",
+                    Data = new Data(new object[] { list1 })},
+                    new Series{
+                    Name = "Owner board 2",
+                    Data = new Data(new object[] { list2 })
+                }});
+
+
+            //pass Chart1Model using ViewBag
+            ViewBag.Chart1Model = chart1;
+
+            return View();
+        }
+
+        public ActionResult Chart2()
+        {
+            var list1 = db.list.Where(u => u.title.Contains("To do")).Count();
+            var list2 = db.list.Where(u => u.title.Contains("Doing")).Count();
+
+            //Create chart Model
+            var chart1 = new Highcharts("Chart1");
+            chart1
+                .InitChart(new Chart() { DefaultSeriesType = ChartTypes.Pie })
+                .SetTitle(new Title() { Text = "To do/Doing" })
+                .SetPlotOptions(new PlotOptions
+                {
+                    Pie = new PlotOptionsPie
+                    {
+                        AllowPointSelect = true,
+                        //Cursor = Cursors.Pointer,
+                        DataLabels = new PlotOptionsPieDataLabels
+                        {
+
+                            Formatter = "function() { return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %'; }"
+                        }
+                    }
+                })
+
+                .SetSeries(new Series
+                {
+                    Type = ChartTypes.Pie,
+
+                    Data = new Data(new object[] { list1, list2 })
+                });
+
+
+            //pass Chart1Model using ViewBag
+            ViewBag.Chart1Model = chart1;
+
+            return View();
+        }
 
         //
         // GET: /List/
